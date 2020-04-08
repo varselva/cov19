70bf563c5755feb7597b53246a6fad84333092cb
VARUN SELVARAJ <varunselvaraj@VARUNs-iMac.local>
2020-04-07 18:42:11 -0700

    inital changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = process.env.PORT;
+var port = process.env.PORT;
+
+if(port == undefined){
+  port = 3000;
+};
 
 app.use(express.static(path.join(__dirname, './../public')));
 
 app.listen(port,() => {
   console.log('Server is listning on port : '+port);
-})
+});
