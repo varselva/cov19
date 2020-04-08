d30f68f578556af6c667a000a2746d0ceb28e68c
Tarun Kumar Jain <tarunkumarjain@Taruns-MacBook-Pro.local>
2020-04-07 17:42:33 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = const port = process.env.PORT;
+
+app.get('/',(req,res) => {
+  res.send('Hi Varun');
+})
+
+app.listen(port,() => {
+  console.log('Server is listning on port : '+port)
+})
