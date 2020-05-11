18112c55732c3e03b73641a4d28ab23ec2b3add0
Tarun Kumar Jain <tarunkumarjain@Taruns-MacBook-Pro.local>
2020-05-10 23:57:37 -0700

    New UI changes for sayuri creations

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 var slideIndex = 1;
+var slideIndexH = 1;
 showSlides1(slideIndex);
 showSlides2(slideIndex);
 showSlides3(slideIndex);
 showSlides4(slideIndex);
+showSlides5(slideIndexH);
 
 function plusSlides1(n) {
   showSlides1(slideIndex += n);
@@ -18,6 +20,10 @@ function plusSlides3(n) {
 
 function plusSlides4(n) {
   showSlides4(slideIndex += n);
+}
+
+function plusSlides5(n) {
+  showSlides5(slideIndexH += n);
 }
 
 
@@ -66,6 +72,25 @@ function showSlides4(n) {
   slides4[slideIndex-1].style.display = "block";
 }
 
+function showSlides5(n) {
+  var i;
+  var slides5 = document.getElementsByClassName("mySlides5");
+  if (n > slides5.length) {slideIndexH = 1}
+  if (n < 1) {slideIndexH = slides5.length}
+  for (i = 0; i < slides5.length; i++) {
+      slides5[i].style.display = "none";
+  }
+  slides5[slideIndexH-1].style.display = "block";
+}
+
 function sendMail() {
   document.getElementById("sendmail").submit();
 }
+
+setInterval(function(){ 
+  plusSlides1(1); 
+  plusSlides2(1); 
+  plusSlides3(1); 
+  plusSlides4(1); 
+  plusSlides5(1); 
+}, 2000);
